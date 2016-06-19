36dd7d092565a050db7f172d44fad36b20c9403a
Devin P <devinfitzsimons@yahoo.com>
2016-06-19 16:45:49 -0400

    Updated the MainActivity to execute the change.

--- app/src/main/java/com/example/devindesktop/cookiesudacity/MainActivity.java
+++ app/src/main/java/com/example/devindesktop/cookiesudacity/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.devindesktop.cookiesudacity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void runSwap(View v){
+        changeImage();
+        changeText();
+    }
+
+    private void changeText() {
+        TextView tv = (TextView) findViewById(R.id.text_to_swap);
+        tv.setText("I'm so full");
+    }
+
+    private void changeImage() {
+        ImageView imageView = (ImageView) findViewById(R.id.cookie_image_view);
+        imageView.setImageResource(R.drawable.after_cookie);
     }
 }
